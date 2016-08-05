9313820984d389ffd55e3d512f61944f45a0b6ed
kuminin <kumin.in@hotmail.com>
2016-08-04 22:19:01 -0700

    Finished ng-model ng-click and ng-repeat section

--- scripts/app.js
+++ scripts/app.js
@@ -3,14 +3,12 @@ angular.module("todoListApp", [])
     $scope.helloWorld = () => {
         console.log("Hello there! This is the helloWorld controller function, in the mainCtrl!");
     };
-})
 
-.controller('coolCtrl', ($scope) => {
-    $scope.whoAmI = () => {
-        console.log("Hello there, this is the coolCtrl function!");
-    };
-
-    $scope.helloWorld = () => {
-        console.log("This is not the main ctrl!");
-    }
+    $scope.todos = [
+        {"name": "clean the house"},
+        {"name": "water the dog"},
+        {"name": "feed the lawn"},
+        {"name": "run"},
+        {"name": "swim"}
+    ];
 });
