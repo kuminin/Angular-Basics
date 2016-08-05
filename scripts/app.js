8b1519e835fd1ddd49bbe9c59e3ea0d7a5c9c221
kuminin <kumin.in@hotmail.com>
2016-08-04 21:31:57 -0700

    Finished  and how to create Controller Section

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,16 @@
-angular.module("todoListApp", []);
+angular.module("todoListApp", [])
+.controller('mainCtrl', ($scope) => {
+    $scope.helloWorld = () => {
+        console.log("Hello there! This is the helloWorld controller function, in the mainCtrl!");
+    };
+})
+
+.controller('coolCtrl', ($scope) => {
+    $scope.whoAmI = () => {
+        console.log("Hello there, this is the coolCtrl function!");
+    };
+
+    $scope.helloWorld = () => {
+        console.log("This is not the main ctrl!");
+    }
+});
--- scripts/hello-world.js
+++
@@ -1,7 +0,0 @@
-angular.module('todoListApp')
-.directive('helloWorld', () => {
-    return {
-        template: "This is the hello world directive!",
-        restrict: "E"
-    };
-});
