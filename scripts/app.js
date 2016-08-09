b4bf8ccc7a7b22caa86ad3bccdb81fbf3b06ee0f
kuminin <kumin.in@hotmail.com>
2016-08-08 20:42:17 -0700

    Finished Using Serices to get Data Section

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,22 @@
 angular.module("todoListApp", [])
-.controller('mainCtrl', ($scope) => {
+.controller('mainCtrl', ($scope, dataService) => {
+    $scope.helloConsole = dataService.helloConsole;
+
     $scope.learningNgChange = () => {
         console.log("An input Changed");
     };
 
-    $scope.todos = [
-        {"name": "clean the house"},
-        {"name": "water the dog"},
-        {"name": "feed the lawn"},
-        {"name": "run"},
-        {"name": "swim"}
-    ];
+    dataService.getTodos(function(res) {
+            console.log(res.data);
+            $scope.todos = res.data;
+    });
+})
+.service('dataService', function($http){
+    this.helloConsole = function() {
+        console.log('This is the hello console service!');
+    };
+
+    this.getTodos = function(callback) {
+        $http.get('mock/todos.json').then(callback);
+    };
 });
